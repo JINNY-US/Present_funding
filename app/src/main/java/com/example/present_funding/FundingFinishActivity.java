aee704fe86f9c356c2a30423ecb71619561713c0
JINNY.US <kyungjin2215@gmail.com>
2022-05-28 01:14:59 +0900

    added funding UI

---
+++ app/src/main/java/com/example/present_funding/FundingFinishActivity.java
@@ -0,0 +1,14 @@
+package com.example.present_funding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FundingFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funding_finish);
+    }
+}
